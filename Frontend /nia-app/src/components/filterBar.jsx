040f3630b69303080cd635a077d3c78354d945b9
Risper <risperwanjikugichia@gmail.com>
2026-08-20 00:42:11 +0300

    Created signup form state management

---
+++ Frontend /nia-app/src/components/filterBar.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+
+
+export default function Signup() {
+    const {signupForm, setSignUpForm} = useState({
+        name : '',
+        email : '',
+        password : ''
+    })
+
+    
+
+}
