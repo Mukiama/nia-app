c7aca5d7b8216e60de916d2247bb63a9c29bbb9b
Risper <risperwanjikugichia@gmail.com>
2026-08-20 00:44:03 +0300

    Handle submit function with prevent default and posting new user details

--- Frontend /nia-app/src/components/filterBar.jsx
+++ Frontend /nia-app/src/components/filterBar.jsx
@@ -8,6 +8,34 @@ export default function Signup() {
         password : ''
     })
 
-    
+    function handleSubmit (event) {
+        event.preventDefault()
+
+        fetch('',
+            {
+                method : 'POST',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body : JSON.stringify(signupForm)
+            }
+        )
+        .then((response) => {
+            if(!response.ok){
+                throw new Error('Error occurred')
+            }else {
+                return response.json()
+            }
+        })
+        .then((data) => {
+            console.log(data)
+            setSignUpForm({
+                name : '',
+                email : '',
+                password : ''
+            })
+        })
+        .catch((error) => alert(error))
+    }
 
 }
