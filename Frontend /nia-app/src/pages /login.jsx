1f3ed76e3129aa701d85e178054d583617757f52
njagihcecil-cpu <njagih.cecil@student.moringaschool.com>
2026-08-20 13:10:52 +0300

    forgot password link

--- Frontend /nia-app/src/pages /login.jsx
+++ Frontend /nia-app/src/pages /login.jsx
@@ -73,6 +73,10 @@ export default function Login() {
         </button>
       </form>
 
+      <a href="/forgot-password">
+        Forgot password?
+      </a>
+
       <p>
         Don't have an account?{" "}
         <a href="/signup">Sign up</a>
