c1b4f40c37b27f495536eb5fd5ad91e39c70717b
Risper <risperwanjikugichia@gmail.com>
2026-08-21 23:52:01 +0300

    Landing page header

--- Frontend /nia-app/src/pages /LandingPage.jsx
+++ Frontend /nia-app/src/pages /LandingPage.jsx
@@ -1,6 +1,16 @@
+import {Link} from 'react-router'
+import './Landing.css'
+
 export default function LandingPage() {
 
     return (
-        <></>
+        <>
+        <div className='landing-page'>
+            <header className='landing-nav'>
+                <span className='landing-logo'>Nia</span>
+                <Link to='/signup'>Sign Up</Link>
+            </header>
+        </div>
+        </>
     )
 }
