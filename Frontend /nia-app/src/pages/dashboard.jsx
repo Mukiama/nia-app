b832ce7b1add00244fa7d6bc2df9f335ed12bb14
Risper <risperwanjikugichia@gmail.com>
2026-08-23 08:14:55 +0300

    Dahboard header

--- Frontend /nia-app/src/pages/dashboard.jsx
+++ Frontend /nia-app/src/pages/dashboard.jsx
@@ -1,8 +1,26 @@
 import './Dashboard.css'
 
-export default function Dashboard() {
+export default function Dashboard({user}) {
+
 
     return (
-        <></>
+        <>
+        <div className='dashboard-page'>
+            <div className='dashboard-card'>
+
+                <div className='dashboard-header'>
+                    <div className='greeting-box'>
+                        <p className='greeting-text'>
+                            Hey {user?.name || 'there'}, ready to discovver with <span className='app-name'>Nia</span>
+                        </p>
+                    </div>
+                    <div className='profile-avatar'>
+                        {user?.name ? user.name.charAt(0).toUpperCase() : '?'}
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        </>
     )
 }
