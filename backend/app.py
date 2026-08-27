93eaba1b909d216ee4a5fac13f3456012f4eb58b
Risper <risperwanjikugichia@gmail.com>
2026-08-27 22:52:36 +0300

    Added endpoints access control

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request, session
 from flask_cors import CORS
 from flask_migrate import Migrate
 from config import Config
 from models import db
 from flask_bcrypt import Bcrypt
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,9 +15,18 @@ migrate = Migrate(app, db)
 CORS(app)
 bcrypt = Bcrypt(app)
 
-@app.route("/")
-def index():
-    return {"message":"backend running"}
+from schemas import user_schema
+
+# @app.route("/")
+# def index():
+#     return {"message":"backend running"}
+
+@app.before_request
+def check_if_logged_in() :
+  open_access = ['signup', 'login', 'check_session']
+
+  if (request.endpoint) not in open_access and (not session.get('user_id')) :
+    return {'error' : '401 Unauthorized'}, 401
 
 if __name__ == "__main__":
   app.run(debug=True)
