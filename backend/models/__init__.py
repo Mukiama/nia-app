4428a74b363257eb67e0ee98ced1985ecc0d0397
Ted karani <ted.karani@student.moringaschool.com>
2026-08-27 22:01:30 +0300

    Expose db and all models from models package __init__

---
+++ backend/models/__init__.py
@@ -0,0 +1,6 @@
+from models.user import db, User
+from models.profile import Profile
+from models.place import Place
+from models.user_place import UserPlace
+from models.category import Category
+from models.item import Item
