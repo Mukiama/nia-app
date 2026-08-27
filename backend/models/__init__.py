746acaf237b5097995837aa5c8a625983e900f06
Ted karani <ted.karani@student.moringaschool.com>
2026-08-27 22:26:05 +0300

    Remove imports for models that don't exist yet, unblock app startup

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,6 +1,3 @@
 from models.user import db, User
 from models.profile import Profile
 from models.place import Place
-from models.user_place import UserPlace
-from models.category import Category
-from models.item import Item
