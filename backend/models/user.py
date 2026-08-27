069f748b3013ed0dfb44c3017f050b64f2167d54
Risper <risperwanjikugichia@gmail.com>
2026-08-27 17:58:58 +0300

    User schema

--- backend/models/user.py
+++ backend/models/user.py
@@ -12,7 +12,7 @@ class User(db.Model) :
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
 
     user_places = db.relationship('Userplace', back_populates='user')
