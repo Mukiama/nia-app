17a5635a5a35be3ae90308200159d0717bc81f98
Risper <risperwanjikugichia@gmail.com>
2026-08-27 23:11:24 +0300

    Signup endpoint

--- backend/app.py
+++ backend/app.py
@@ -27,10 +27,14 @@ bcrypt = Bcrypt(app)
 >>>>>>> dfd37d6 (Set password hash)
 
 from schemas import user_schema
+from models import user
 
 # @app.route("/")
 # def index():
 #     return {"message":"backend running"}
+
+User = user.User
+UserSchema = user_schema.UserSchema
 
 @app.before_request
 def check_if_logged_in() :
@@ -38,6 +42,28 @@ def check_if_logged_in() :
 
   if (request.endpoint) not in open_access and (not session.get('user_id')) :
     return {'error' : '401 Unauthorized'}, 401
+
+
+class Signup(Resource) :
+  def post(self) :
+
+    name = request.get_json()['username']
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+
+    user = User(
+      name = name,
+      email = email
+    )
+    user.password_hash = password
+
+    try :
+      db.session.add(user)
+      db.session.commit()
+      session['user_id'] = user.id
+      return UserSchema().dump(user), 200
+    except IntegrityError :
+      return {'error' : '422 Unprocessed Entity'}, 422
 
 if __name__ == "__main__":
   app.run(debug=True)
