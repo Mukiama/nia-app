b9b5606d5e6b015ef45f6cc56c0d95a0224423ce
Walter Mahalon <malonwalter04@gmail.com>
2026-08-27 16:43:14 +0300

    Added items field and description

--- backend/models/items.py
+++ backend/models/items.py
@@ -7,3 +7,5 @@ db = SQLAlchemy(metadata=metadata)
 class Item(db.Model):
     __tablename__ = "items"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.Strings, nullable=False)
