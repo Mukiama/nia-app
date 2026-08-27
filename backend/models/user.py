3698308f51398b054b063c92d44443da49b903f0
Risper <risperwanjikugichia@gmail.com>
2026-08-27 17:35:45 +0300

    Added password length table validator

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,7 @@
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
+from sqlalychemy.orm import validates
+from email_validator import validate_email, EmailNotValidError
 
 metadata = MetaData()
 
@@ -9,8 +11,16 @@ class User(db.Model) :
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
 
     user_places = db.relationship('Userplace', back_populates='user')
+
+    __table_args__ = (
+        db.CheckConstraint('length(password) >= 6')
+    )
+
+    
+    def __repr__(self) :
+        return f'<User {self.id} : {self.name}>'
