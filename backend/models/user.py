364ef626749ff09887a851230b78fa0e83ffb05f
Risper <risperwanjikugichia@gmail.com>
2026-08-27 17:12:24 +0300

    Added user columns

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,3 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model) :
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    user_places = db.relationship('Userplace', back_populates='user')
