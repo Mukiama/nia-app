fd255d619556001324b8014d7e34c8170ffad8fc
Mukiama <mukiamamwereria@gmail.com>
2026-08-27 12:33:54 +0300

    feat:add profile models

---
+++ backend/models/profile.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+metadata = MetaData()
+
+db = SQLAlchemy(metadata=metadata)
+
+class Profile(db.Model):
+    __tablename__ = "profiles"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    interests = db.Column(db.String, nullable=False)
+    budget = db.Column(db.String, nullable=False)
+    company = db.Column(db.String, nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False,
+        unique=True,
+    )
