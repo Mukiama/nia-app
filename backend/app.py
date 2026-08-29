0fa199620dd6d56e9dc67224f5c947d7b07dfc25
Mukiama <mukiamamwereria@gmail.com>
2026-08-29 16:14:27 +0300

    importing shared database to app.py

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,18 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from config import Config
 from models import db
-from routes.place_routes import place_bp
 from flask_bcrypt import Bcrypt
 from flask_restful import Api
-
 from flask_jwt_extended import JWTManager
 import os
 from dotenv import load_dotenv
+
+from routes.place_routes import place_bp
+from routes.profile_routes import profile_bp
+from routes.category_routes import category_bp
+from routes.items_routes import item_bp
+from routes.user_routes import Signup, Login, Logout, Verification
+
 
 load_dotenv()
 
