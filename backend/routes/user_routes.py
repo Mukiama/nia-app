010bfb7f606be948746a636c89cb34c54fc37601
njagih.cecil <njagih.cecil@student.moringaschool.com>
2026-08-30 21:07:27 +0300

    fix login query, dead verification endpoint, and drop the app.py import

--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,74 +1,78 @@
-from flask import request, session, make_response, jsonify
+from flask import request, make_response, jsonify
 from flask_restful import Resource
+from sqlalchemy import and_
 from sqlalchemy.exc import IntegrityError
-from app import app, db, api, jwt
-from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request
+from flask_jwt_extended import create_access_token, get_jwt_identity
 
-from schemas import user_schema
+from models import db
 from models import user
+from schemas import user_schema
 
 User = user.User
 UserSchema = user_schema.UserSchema
 
 
-@app.before_request
-def check_if_logged_in() :
-  open_access = ['signup', 'login', 'check_session']
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        name = data['name']
+        email = data['email']
+        password = data['password']
 
-  if (request.endpoint) not in open_access and (not verify_jwt_in_request()) :
-    return {'error' : '401 Unauthorized'}, 401
+        if not password or len(password) < 6:
+            return {'error': 'Password must be at least 6 characters'}, 422
 
+        new_user = User(
+            name=name,
+            email=email,
+        )
+        new_user.password_hash = password
 
-class Signup(Resource) :
-  def post(self) :
-
-    name = request.get_json()['name']
-    email = request.get_json()['email']
-    password = request.get_json()['password']
-
-    user = User(
-      name = name,
-      email = email
-    )
-    user.password_hash = password
-
-    try :
-      db.session.add(user)
-      db.session.commit()
-      access_token = create_access_token(identity=int(user.id))
-      return make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)      
-    except IntegrityError :
-      return {'error' : '422 Unprocessed Entity'}, 422
-
-
-class Login(Resource) :
-  def post(self) :
-    name = request.get_json()['name'] 
-    email = request.get_json()['email']
-
-    user = User.query.filter(User.name == name and User.email == email).first()
-
-    password = request.get_json()['password']
-
-    if user and user.authenticate(password) :
-      access_token = create_access_token(identity=int(user.id))
-      return make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
-
-    else :
-      return {'error' : '401 Unauthorized'}, 401
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            access_token = create_access_token(identity=int(new_user.id))
+            return make_response(
+                jsonify(token=access_token, user=UserSchema().dump(new_user)),
+                200,
+            )
+        except IntegrityError:
+            db.session.rollback()
+            return {'error': '422 Unprocessable Entity'}, 422
 
 
-class Verification(Resource) :
-  def check_session(self) :
-    user_id = get_jwt_identity()
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        name = data['name']
+        email = data['email']
+        password = data['password']
 
-    user = User.query.filter(User.id == user_id).first()
-    return UserSchema().dump(user), 200
+        found_user = User.query.filter(
+            and_(User.name == name, User.email == email)
+        ).first()
+
+        if found_user and found_user.authenticate(password):
+            access_token = create_access_token(identity=int(found_user.id))
+            return make_response(
+                jsonify(token=access_token, user=UserSchema().dump(found_user)),
+                200,
+            )
+        else:
+            return {'error': '401 Unauthorized'}, 401
 
 
-# The client will be resposible of removing their own JWT tokens
-class Logout(Resource) :
-  def post(self) :
-    return {}, 204
+class Verification(Resource):
+    def get(self):
+        user_id = get_jwt_identity()
+        found_user = User.query.filter(User.id == user_id).first()
+
+        if not found_user:
+            return {'error': 'User not found'}, 404
+
+        return UserSchema().dump(found_user), 200
 
 
+class Logout(Resource):
+    def post(self):
+        return {}, 204
