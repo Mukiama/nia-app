1bd5fad0a1bcd8d83d50eed50e788ce648581050
Walter Mahalon <malonwalter04@gmail.com>
2026-08-30 22:28:36 +0300

    Load category seed data

--- backend/seeds/seed.py
+++ backend/seeds/seed.py
@@ -18,6 +18,9 @@ with app.app_context():
     with open(ITEM_FILE, "r", encoding="utf-8") as f:
         items = json.load(f)
 
+    with open(CATEGORY_FILE, "r", encoding="utf-8") as f:
+        categories = json.load(f)
+
     for profile_data in profiles:
 
         profile = Profile(
